cd9eafe1bc3c89933e9b8d0590907ca13e71e79b
ImEldin <popara.eldin@gmail.com>
2025-04-01 00:02:10 +0200

    Added env loading

--- src/main/java/com/team5/career_progression_app/CareerProgressionAppApplication.java
+++ src/main/java/com/team5/career_progression_app/CareerProgressionAppApplication.java
@@ -13,7 +13,6 @@ public class CareerProgressionAppApplication {
 		Dotenv dotenv = Dotenv.load();
 
 		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
 		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
