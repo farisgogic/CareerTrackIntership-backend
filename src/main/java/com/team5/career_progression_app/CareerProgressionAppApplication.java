326179cd9ba55cbf6da828235369ba9d4b82237d
ImEldin <popara.eldin@gmail.com>
2025-03-31 23:50:52 +0200

    Added database connection

--- src/main/java/com/team5/career_progression_app/CareerProgressionAppApplication.java
+++ src/main/java/com/team5/career_progression_app/CareerProgressionAppApplication.java
@@ -2,11 +2,22 @@ package com.team5.career_progression_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CareerProgressionAppApplication {
 
 	public static void main(String[] args) {
+
+		//Ucitavanje environment varijabli iz .env
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(CareerProgressionAppApplication.class, args);
 	}
 
