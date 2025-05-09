3e3c80c0dc6524dd29bd9591bea2e417ef516fda
nobucina1 <92829341+nobucina1@users.noreply.github.com>
2025-05-09 15:10:13 +0200

    All roles view (#15)
    * Add roles management view with create, edit and delete functionality
    
    * Fixed review comments

--- src/main/java/com/team5/career_progression_app/service/RoleService.java
+++ src/main/java/com/team5/career_progression_app/service/RoleService.java
@@ -1,9 +1,14 @@
 package com.team5.career_progression_app.service;
 
+import com.team5.career_progression_app.dto.RoleDTO;
+
 import java.util.List;
 
 public interface RoleService {
 
     List<String> getAllPermissionNames();
     void insertRole(String roleName, List<String> permissionNames);
+    public List<RoleDTO> getAllRoles();
+    void updateRole(Integer id, String newName, List<String> permissionNames);
+    public void deleteRole(Integer id);
 }
