81c3ec3a29bab065489393fd3e5c45f8a8ef2ef3
Faris Gogic <faris.gogic@edu.fit.ba>
2025-05-14 16:38:55 +0200

    Refactor PositionController and PositionService for improved position management

--- src/main/java/com/team5/career_progression_app/restController/PositionController.java
+++ src/main/java/com/team5/career_progression_app/restController/PositionController.java
@@ -2,7 +2,13 @@ package com.team5.career_progression_app.restController;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +27,20 @@ public class PositionController {
     @GetMapping
     public List<PositionDTO> getAllPositions(){
         return positionService.getAllPositions();
+    }
+
+    @PostMapping
+    public ResponseEntity<PositionDTO> createPosition(@RequestBody PositionDTO positionDTO) {
+        return new ResponseEntity<>(positionService.createPosition(positionDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PositionDTO> updatePosition(@PathVariable int id, @RequestBody PositionDTO positionDTO) {
+        return ResponseEntity.ok(positionService.updatePosition(id, positionDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PositionDTO> getPositionById(@PathVariable int id) {
+        return ResponseEntity.ok(positionService.getPositionById(id));
     }
 }
