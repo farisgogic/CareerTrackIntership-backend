43dee8cb8119373487be3a7394752b862dc75706
Faris Gogic <faris.gogic@edu.fit.ba>
2025-05-14 15:33:47 +0200

    Update TemplateController.java

--- src/main/java/com/team5/career_progression_app/restController/TemplateController.java
+++ src/main/java/com/team5/career_progression_app/restController/TemplateController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/template")
+@RequestMapping("/api/template")
 @AllArgsConstructor
 public class TemplateController {
 
