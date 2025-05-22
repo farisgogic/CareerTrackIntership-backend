c7025041195159c88ca4573871f7741781556f1d
ImEldin <popara.eldin@gmail.com>
2025-05-22 11:54:52 +0200

    resolved comments

--- src/main/java/com/team5/career_progression_app/restController/TeamController.java
+++ src/main/java/com/team5/career_progression_app/restController/TeamController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/teams")
+@RequestMapping("/teams")
 public class TeamController {
 
     private final TeamService teamService;
--- src/main/java/com/team5/career_progression_app/restController/TemplateController.java
+++ src/main/java/com/team5/career_progression_app/restController/TemplateController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/template")
+@RequestMapping("/template")
 @AllArgsConstructor
 public class TemplateController {
 
